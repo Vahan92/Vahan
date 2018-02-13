1483d1c902028a97f84ffd4c8e7d610ba1c4c47d
Vahan92 <vahan.kalenteryan@gmail.com>
2018-02-13 17:13:29 +0400

    Added

---
+++ js/my.js
@@ -0,0 +1,69 @@
+let myApp = function() {
+    let max = 100;
+    let min = 4;
+    return {
+
+        getId: function(params) {
+            return document.getElementById(params);
+        },
+
+        printing: function(inpVal) {
+            let print = myApp.getId("print");
+            if (typeof print !== null) {
+                print.textContent = "Number of cells: " + inpVal.value;
+            }
+        },
+
+        draw: function() {
+            let inputVal = myApp.getId("num").value;
+            let forCells = myApp.getId("forCells");
+            if (inputVal < min || inputVal > max) {
+                alert(`Please enter a number between ${min} and ${max}, or you are going to get this annoying alert notification :)`);
+            } else {
+                var i = 0;
+                let fragment = document.createDocumentFragment();
+
+                while (i < inputVal) {
+                    let newSpan = document.createElement('span');
+                    newSpan.innerText = i;
+                    let newDiv = document.createElement('div');
+                    newDiv.appendChild(newSpan);
+                    fragment.appendChild(newDiv);
+                    i++;
+                }
+                myApp.getId('resets').disabled = false;
+                myApp.getId('colourises').disabled = false;
+                myApp.getId('draws').disabled = true;
+                forCells.appendChild(fragment);
+            }
+        },
+
+        getRandomColor: function() {
+            let letters = '0123456789ABCDEF';
+            let color = '#';
+            for (var i = 0; i < 6; i++) {
+                color += letters[Math.floor(Math.random() * 16)];
+            }
+            return color;
+        },
+
+        colourise: function() {
+            let toColour = forCells.children;
+            var i;
+            for (i = 0; i < toColour.length; i++) {
+                (function(i) {
+                    setTimeout(function() {
+                        toColour[i].style.backgroundColor = myApp.getRandomColor();
+                        toColour[i].style.transition = "all 1.5s";
+                    }, 1500 * i);
+                })(i);
+            };
+        },
+
+        reset: function() {
+            while (forCells.firstChild) {
+                forCells.removeChild(forCells.firstChild);
+            }
+        }
+    }
+}();
